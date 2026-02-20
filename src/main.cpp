ebd5fa678f7aeca9fc59639f05fa4fb3fa7ce346
g5n9u1-coder <shashank.singh2_cs.aiml@gla.ac.in>
2026-02-20 09:14:41 +0530

    //

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#define trigPin 9
+#define echoPin 10
+void setup() {
+    // write your initialization code here
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+}
+
+void loop() {
+    // write your code here
+    digitalWrite(trigPin,LOW);
+    delayMicroseconds(2);
+    digitalWrite(echoPin,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin,LOW);
+    long duration = pulseIn(echoPin,HIGH);
+    long distance = duration * 0.034/2;
+    Serial.print("Distance : ");
+    Serial.println(distance);
+    Serial.println(" cm");
+    delay(1000);
+}
